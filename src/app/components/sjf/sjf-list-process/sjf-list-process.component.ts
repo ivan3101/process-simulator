0d868f607b27f62cf0921e70e8442fad16491078
Ivan De Menezes <ivandmenezes@gmail.com>
2018-03-21 01:13:21 -0400

    Implemented SJF list processes

--- src/app/components/sjf/sjf-list-process/sjf-list-process.component.ts
+++ src/app/components/sjf/sjf-list-process/sjf-list-process.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {SjfService} from '../../../services/sjf.service';
 
 @Component({
   selector: 'app-sjf-list-process',
   templateUrl: './sjf-list-process.component.html',
   styleUrls: ['./sjf-list-process.component.css']
 })
-export class SjfListProcessComponent implements OnInit {
+export class SjfListProcessComponent implements OnInit, OnDestroy {
+  fields;
+  processes;
+  executing;
+  addProcessSubscription: Subscription;
+  processExecutingSubscription: Subscription;
+  finishedProcessSubscription: Subscription;
+  finishedProcessesSubscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sjfService: SjfService) {
+    this.executing = false;
+    this.fields = ['Nombre', 'Espacio ocupado', 'Tiempo de ejecución'];
+    this.processes = this.sjfService.getAllProcesses();
   }
 
+  ngOnInit() {
+    this.addProcessSubscription = this.sjfService.addProcessEvent.subscribe(value => {
+      if (value) this.processes = this.sjfService.getAllProcesses();
+    });
+
+    this.processExecutingSubscription = this.sjfService.processExecuting.subscribe(process => {
+      this.processes[this.processes.indexOf(process)].state = 'Ejecutando';
+    });
+
+    this.finishedProcessSubscription = this.sjfService.finishedProcess.subscribe(process => {
+      this.processes[this.processes.indexOf(process)].state = 'Finalizado';
+    });
+
+    this.finishedProcessesSubscription = this.sjfService.finishedProcesses.subscribe(value => {
+      if (value) {
+        this.executing = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.addProcessSubscription.unsubscribe();
+  }
+
+  onExecuteProcesses() {
+    this.executing = true;
+    this.sjfService.executeProcesses();
+  }
+
+  onClearProcesses() {
+    this.sjfService.clearProcesses();
+  }
 }
