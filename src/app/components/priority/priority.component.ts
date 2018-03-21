ab36bdccc5018de535f5ba228ec285adb5408616
Ivan De Menezes <ivandmenezes@gmail.com>
2018-03-21 02:01:52 -0400

    Added priority component

--- src/app/components/priority/priority.component.ts
+++ src/app/components/priority/priority.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PriorityService} from '../../services/priority.service';
 
 @Component({
   selector: 'app-priority',
   templateUrl: './priority.component.html',
   styleUrls: ['./priority.component.css']
 })
-export class PriorityComponent implements OnInit {
+export class PriorityComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private priorityService: PriorityService) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.priorityService.clearProcesses()
   }
 
 }
